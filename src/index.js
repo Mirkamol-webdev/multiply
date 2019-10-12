af120a36f07e8a8d51d63495e2099ddb8eaa0c3a
Mirkamol <msd022m@mail.ru>
2019-10-12 15:28:29 +0500

    feat: implement multiply task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    // your solution
+    var x =  BigInt(first);
+    var y =  BigInt(second);
+    var c =  (x * y);
+    return c.toString();
 }
